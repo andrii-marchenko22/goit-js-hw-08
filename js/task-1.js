24ba9a202d17e27754988b87388dbde7826c928b
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-03-16 02:10:38 +0200

    add js html css files

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll(".item");
-console.log(`Number of categories: ${elements.length}`);
-
-elements.forEach(element => {
-    const textTitle = element.querySelector("h2").textContent;
-    const list = element.querySelectorAll("li").length;
-    console.log(`Category: ${textTitle}`);
-    console.log(`Elements: ${list}`);
-});
